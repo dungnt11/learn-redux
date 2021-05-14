91154793809fa6e94300df4e7dadf48ceaca3ada
JzunG <nihilism.core@gmail.com>
2021-05-14 15:16:48 +0000

    Feat: gọi counter trong header

--- src/features/header/index.js
+++ src/features/header/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { changeHeader } from './store';
+import { increment } from '../counter/counterSlice';
 
 export function Header() {
   const headerStore = useSelector((state) => state.header);
   const dispatch = useDispatch();
 
+  function changeHeaderFn({ target }) {
+    dispatch(changeHeader(target.value));
+  }
+
+  function incrementFn() {
+    dispatch(increment()); 
+  }
+
   return (
     <div>
-      <input value={headerStore.header} onChange={({ target }) => dispatch(changeHeader(target.value))} />
+      <input value={headerStore.header} onChange={changeHeaderFn} />
+      <button onClick={incrementFn}>increment</button>
     </div>
   );
 }
